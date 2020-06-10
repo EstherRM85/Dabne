2c87f9b2ebb615ee779fffafbc4bc22479d5ddaa
Diana Toledo <diaelitg@gmail.com>
2020-06-10 12:19:07 +0200

    Codigo php

--- formulario-dabne/formulario_01.php
+++ formulario-dabne/formulario_01.php
@@ -1,18 +1,18 @@
-
-
-
 <?php
+if(isset($_POST['enviar'])){
+if(!empty($_POST['intereses'])) {
 
 $archivo = fopen("resultados_formulario.txt","a") or die ("Error al crear");
-
 $nombre = $_POST ['nombre'];
 $intereses = $_POST ['intereses'];
-
-
 fwrite($archivo, $nombre);
 fwrite($archivo, "\n");
-fwrite($archivo, $intereses);
+foreach( $intereses as $interes) {
+
+    fwrite($archivo, $interes); 
+    fwrite($archivo, "\n");
+}
 fwrite($archivo, "\n");
-
- ?>
-
+}
+}
+?>
