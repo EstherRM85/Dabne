17f5cf165daf9a2f55d6f64b1782ec4337523911
Diana Toledo <diaelitg@gmail.com>
2020-06-11 12:41:42 +0200

    Ejercicio Boletines

---
+++ boletines/boletin.js
@@ -0,0 +1,14 @@
+/*var aboletines = document.getElementsByTagName('h3');
+
+
+
+aboletines.forEach(funtion() {
+    /*aboletin.onclick = function() {
+        var boletin = document.getElementById('lblt2019');
+        boletin.style.display = 'inline';
+    }
+});*/
+function mostrarBoletines(){
+	document.getElementById('lblt2019').style.display = 'block';
+}
+
